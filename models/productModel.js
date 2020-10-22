f12917ebf82758355ee452d2ab39773c65c14d7b
Andy <andrew.rutherford.6@gmail.com>
2020-10-22 11:52:45 +0200

    Product model

---
+++ models/productModel.js
@@ -0,0 +1,9 @@
+const products = require('../data/products');
+
+function findAll() {
+  return new Promise((resolve, reject) => {
+    resolve(products);
+  });
+}
+
+module.exports = { findAll };
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
-
-const products = require('./data/products');
+const { getProducts } = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(products));
+  if (req.url === '/api/products' && req.method === 'GET') {
+    getProducts(req, res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Not found.' }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
